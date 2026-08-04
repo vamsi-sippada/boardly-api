e1d057d357d41b26adc43cd45f3f6c4dd152630a
wraith19 <svammsi0901@gmail.com>
2026-08-04 09:55:19 +0530

    fix: remove SECURE_SSL_REDIRECT for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -158,7 +158,7 @@ if os.environ.get('RENDER'):
             conn_health_checks=True,
         )
     }
-    SECURE_SSL_REDIRECT        = True
+    # SECURE_SSL_REDIRECT        = True
     SECURE_PROXY_SSL_HEADER    = ('HTTP_X_FORWARDED_PROTO', 'https')
     SESSION_COOKIE_SECURE      = True
     CSRF_COOKIE_SECURE         = True
